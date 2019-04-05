ded4cfea524f00647c657ba005adff77f73c3952
Eloypripan <34416018+Eloypripan@users.noreply.github.com>
2019-04-05 18:03:27 +0200

    Update mencionar_bot.py

--- mencionar_bot.py
+++ mencionar_bot.py
@@ -24,6 +24,7 @@ commands = {  # command description used in the "help" command
 }
 
 bot = telebot.TeleBot(TOKEN) # Creamos el objeto de nuestro bot.
+
 #############################################
 #Listener
 def listener(messages): # Con esto, estamos definiendo una función llamada 'listener', que recibe como parámetro un dato llamado 'messages'.
@@ -35,36 +36,31 @@ def listener(messages): # Con esto, estamos definiendo una función llamada 'lis
 bot.set_update_listener(listener) # Así, le decimos al bot que utilice como función escuchadora nuestra función 'listener' declarada arriba.
 #############################################
 #Funciones
+
+##START
 @bot.message_handler(commands=['start'])
 def command_start(m):
-
 	cid = m.chat.id
 	bot.send_message(cid, "Arrancando Pingüeinstein...\n¡Buenas! ¿En qué puedo servirle?")
 	command_help(m)
 
+##HELP
 @bot.message_handler(commands=['help'])
 def command_help(m):
-
     cid = m.chat.id
-
     help_text = "Los siguientes comandos están disponibles:\n\n"
-
     for key in commands:  # generate help text out of the commands dictionary defined at the top
         help_text += "/" + key + ": "
         help_text += commands[key] + "\n"
-
     bot.send_message(cid, help_text)  # send the generated help page
 
-@bot.message_handler(commands=['mencionar']) # Indicamos que lo siguiente va a controlar el comando '/chiste'
-def command_comentario(m): # Definimos una función que resuleva lo que necesitemos.
-	cid = m.chat.id # Guardamos el ID de la conversación para poder responder.
-	bot.send_message( cid,)
-  
-@bot.message_handler(func=lambda message: message.content_type == "text" and "@all" in message.text.lower())
-def all:
-    bot.reply_to(m, )
+##STATUS
+@bot.message_handler(commands=['status'])
+def command_staus(m):
+    cid = m.chat.id
+    bot.send_message(cid, "Estoy 100% operativo")
 
-
+##MSG	
 @bot.message_handler(commands=['msg'])
 def send_mensaje(m):
     text = m.text[len("/msg "):]
@@ -80,7 +76,32 @@ def send_mensaje(m):
 
     bot.send_message(cid, mensaje)
 
-  
-  
-############################################33  
+##MENCIONAR
+@bot.message_handler(commands=['mencionar']) # Indicamos que lo siguiente va a controlar el comando '/chiste'
+def command_mencionar(m): # Definimos una función que resuleva lo que necesitemos.
+	cid = m.chat.id # Guardamos el ID de la conversación para poder responder.
+	bot.send_message( cid,)
 
+##ALL
+@bot.message_handler(func=lambda message: message.content_type == "text" and "@all" in message.text.lower())
+def all:
+    bot.reply_to(m, )
+
+############################################
+#Peticiones
+##LOGING TO TELEGRAM
+logger = logging.getLogger(__name__)
+
+while True:
+
+    try:
+
+            bot.polling(none_stop=True)
+
+    except Exception as err:
+
+            logger.error(err)
+
+            time.sleep(10)
+
+            print('Error en la conexión')
